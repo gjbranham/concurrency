1c0a0b14d577c75b4c6e845bc345efd4096cf04d
Griffin Branham <griffin@apricitycode.com>
2023-09-29 20:04:59 -0700

    Go func in for loop

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
-package main
-
-import "fmt"
-
 // Implement a function that, given an array of URLs and a download function,
 // downloads all the data from the urls, merges it into a single dictionary of {url:data}
 // and return a method with parallelism (concurrency)
+
+package main
+
+import "fmt"
 
 type urlData struct {
 	url  string
@@ -16,8 +16,9 @@ func main() {
 
 	c := make(chan urlData)
 
-	go get(urls[:len(urls)/2], c)
-	go get(urls[len(urls)/2:], c)
+	for _, url := range urls {
+		go get(url, c)
+	}
 
 	out := make(map[string]string)
 
@@ -29,15 +30,13 @@ func main() {
 	for k, v := range out {
 		fmt.Printf("url: %v, data: %v\n", k, v)
 	}
-
 }
 
-func get(urls []string, c chan urlData) {
-	for _, u := range urls {
-		data := urlData{
-			url:  u,
-			data: fmt.Sprintf("data for url %v", u),
-		}
-		c <- data
+func get(url string, c chan urlData) {
+	data := urlData{
+		url:  url,
+		data: fmt.Sprintf("data for url %v", url),
 	}
+	c <- data
+
 }
